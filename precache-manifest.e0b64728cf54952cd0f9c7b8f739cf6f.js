4b2b0c5270bd31e4113044b4b9f192f052751c1f
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-06-30 16:08:51 -0700

    Updates

---
+++ precache-manifest.e0b64728cf54952cd0f9c7b8f739cf6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cfff32828c8e9d88fc2a9f6a5518ac",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "53b6123c52ac12967b0d",
+    "url": "/pokedex/static/css/main.1effef40.chunk.css"
+  },
+  {
+    "revision": "f6c256ca51b9c056827b",
+    "url": "/pokedex/static/js/2.0b85763f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.0b85763f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b6123c52ac12967b0d",
+    "url": "/pokedex/static/js/main.6d32c11d.chunk.js"
+  },
+  {
+    "revision": "dd6bddbc3b7842f642db",
+    "url": "/pokedex/static/js/runtime-main.ed7addec.js"
+  }
+]);
